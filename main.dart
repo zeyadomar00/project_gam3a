88e76746927540c509640531970d571089f1b38c
zeyadomar00 <149247115+zeyadomar00@users.noreply.github.com>
2024-03-23 21:29:23 -0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:application5/pages/login.dart';
+import 'package:application5/pages/signup.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: (LoginPage()),
+      routes:{
+        "login":(context) => LoginPage(),
+        "SignUp": (context) => SignUp(),
+      },
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
